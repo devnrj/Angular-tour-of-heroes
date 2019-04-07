aedd3d85f2f76605518437fe8fb8a50f3d6880e6
Neeraj Kumar <neeraj.kumar03@nagarro.com>
2019-04-07 10:46:41 +0530

    Heroes list done

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Windstorm' },
+  { id: 2, name: "Machiato" },
+  { id: 3, name: "Winizooma" },
+  { id: 4, name: "Boombasta" },
+  { id: 5, name: "Elasticman" },
+  { id: 6, name: "Celerista" },
+  { id: 7, name: "Dr. IQ" },
+  { id: 8, name: "Dexter" },
+  { id: 9, name: "Emoter" },
+  { id: 10, name: "Neeraj" }
+];
